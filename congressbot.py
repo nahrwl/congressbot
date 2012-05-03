8709860cad8923921ec8842f65174eba4c99fab9
Kevin O'Shea <kevin@myuid.com>
2012-05-03 10:58:25 +0000

    2 spaces will do

--- congressbot.py
+++ congressbot.py
@@ -13,67 +13,67 @@ house_collection = congress_db.wc_house_today
 
 template = Template(open('post_template.md').read())
 
-
 def parse(ignore_duty=True, ignore_resolutions=True):
-    govfeed = feedparser.parse('http://www.govtrack.us/events/events.rss?'
-                               'feeds=misc%3Aintroducedbills')
+  govfeed = feedparser.parse('http://www.govtrack.us/events/events.rss?'
+    'feeds=misc%3Aintroducedbills')
 
-    r = Reddit(user_agent='WatchingCongress/1.0')
-    r.login('congressbot', '<BOTPASS>')
+  r = Reddit(user_agent='WatchingCongress/1.0')
+  r.login('congressbot', '<BOTPASS>')
 
-    for entry in govfeed.entries:
-        if not entry['guid'].find('guid'):
-            logging.info("Couldn't find GUID")
-            continue
+  for entry in govfeed.entries:
+    if not entry['guid'].find('guid'):
+      logging.info("Couldn't find GUID")
+      continue
 
-        if not entry['title']:
-            logging.info("No title for bill: {}".format(entry['guid']))
-            continue
+    if not entry['title']:
+      logging.info("No title for bill: {}".format(entry['guid']))
+      continue
 
-        if house_collection.find_one({'guid': entry['guid']}):
-            logging.info("Already created story: {}".format(entry['title']))
-            continue
+    if house_collection.find_one({'guid': entry['guid']}):
+      logging.info("Already created story: {}".format(entry['title']))
+      continue
 
-        if ignore_duty and 'duty' in entry['title'] and 'temporar' in entry['title']:
-            logging.info("Ignored boring bill: {}".format(entry['title']))
-            continue
+    if ignore_duty and 'duty' in entry['title'] and 'temporar' in entry['title']:
+      logging.info("Ignored boring bill: {}".format(entry['title']))
+      continue
 
-        if ignore_resolutions and '.Res' in entry['title']:
-            logging.info("Ignored resolution: {}".format(entry['title']))
-            continue
+    if ignore_resolutions and '.Res' in entry['title']:
+      logging.info("Ignored resolution: {}".format(entry['title']))
+      continue
 
-        record = {
-            'title': entry['title'],
-            'description': entry['description'],
-            'link': entry['link'],
-            'guid': entry['guid'],
-        }
+    record = {
+      'title': entry['title'],
+      'description': entry['description'],
+      'link': entry['link'],
+      'guid': entry['guid'],
+    }
 
-        bill_number = entry['title'].split(':')[0]
-        news_stories = find_news_stories(bill_number)
+    bill_number = entry['title'].split(':')[0]
+    news_stories = find_news_stories(bill_number)
 
-        try:
-            text = template.render(description=entry['description'],
-                                   link=entry['link'],
-                                   news_stories=news_stories)
-            r.submit('watchingcongress', entry['title'], text=text)
-            house_collection.insert(record)
-            logging.info("Created story: {}".format(entry['title']))
-        except Exception as e:
-            logging.error("Exception occured: {}".format(unicode(e)))
-            time.sleep(2)
-
+    try:
+      text = template.render(description=entry['description'],
+                   link=entry['link'],
+                   news_stories=news_stories)
+      r.submit('watchingcongress', entry['title'], text=text)
+      house_collection.insert(record)
+      logging.info("Created story: {}".format(entry['title']))
+    except Exception as e:
+      logging.error("Exception occured: {}".format(unicode(e)))
+      time.sleep(2)
 
 def find_news_stories(query):
-    query = urllib.quote_plus(query)
-    news_feed = feedparser.parse('https://news.google.com/news/feeds?q="%s"' % query)
-    news_items = []
-    for entry in news_feed.entries:
-        if '(subscription)' not in entry['title']: # ignore subscription results
-            link = entry['link'][entry['link'].find('url=') + 4:]
-            news_items.append({'title': entry['title'], 'link': link})
-    return news_items
+  query = urllib.quote_plus(query)
+  news_feed = feedparser.parse('https://news.google.com/news/feeds?q="%s"' % query)
+  news_items = []
+  for entry in news_feed.entries:
+    if '(subscription)' not in entry['title']: # ignore subscription results
+      link = entry['link'][entry['link'].find('url=') + 4:]
+      news_items.append({'title': entry['title'], 'link': link})
+  return news_items
 
 
 if __name__ == '__main__':
-    parse()
+  parse()
+
+# vim: set filetype=python expandtab tabstop=2 shiftwidth=2 autoindent smartindent:
