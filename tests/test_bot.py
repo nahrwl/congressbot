5a4f52348d8174e9cb3c4c0b8bfe0baa50f70f31
Peter Baehr <baehr@abiresearch.com>
2012-05-03 09:51:53 -0400

    Add test for google feed

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -9,3 +9,7 @@ def test_feed_parse(reddit_mock, house_mock):
     house_mock.find_one.return_value = False
     congressbot.parse()
     assert False
+
+def test_google_feed():
+    # Will need to be updated in the event of lasting world peace
+    assert congressbot.find_news_stories('war')
